da0bacaf1068c6a1b3bc48cb3a3c6abb7d59d6ef
Elizaveta Zalomnova <handlessdude@gmail.com>
2025-05-04 23:13:12 +0300

    add gl-matrix

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
+import { ReadonlyVec3 } from 'gl-matrix';
+import { glMatrix, mat4 } from 'gl-matrix';
+
 console.log('hello');
+
+const identity = new Float32Array(16);
+mat4.identity(identity);
+
+const vec: ReadonlyVec3 = [1, 1, 1];
+const zRotationMatrix = new Float32Array(16);
+mat4.rotate(zRotationMatrix, identity, Math.PI / 6, [0, 0, 1]);
+
+
+const fovy = glMatrix.toRadian(45);
 
 const a: number = 6;
 
