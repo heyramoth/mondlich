8bed7aa02cbd5fb4c1f83b929935ac297cbd0302
Elizaveta Zalomnova <handlessdude@gmail.com>
2025-06-01 02:55:48 +0300

    update workers bundling

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,3 +2,8 @@ declare module '*.png' {
   const src: string;
   export default src;
 }
+
+declare module '*.worker.js?worker&url' {
+  const url: string;
+  export default url;
+}
--- src/lib/core/workerManager/domain/workerWrapper.ts
+++ src/lib/core/workerManager/domain/workerWrapper.ts
@@ -9,12 +9,23 @@ type TWorkerReturnValue = {
   aliveStatus: Uint8Array,
 };
 
+const logWorkerError = (event: ErrorEvent) => {
+  console.error('Worker error:', {
+    message: event.message,
+    filename: event.filename,
+    lineno: event.lineno,
+    colno: event.colno,
+    error: event.error,
+  });
+};
+
 export class WorkerWrapper {
   private worker: Worker;
   private taskCount: number = 0;
 
-  constructor(workerScript: URL) {
-    this.worker = new Worker(workerScript, { type: 'module' });
+  constructor(workerUrl: URL) {
+    this.worker = new Worker(workerUrl, { type: 'module' });
+    this.worker.onerror = logWorkerError;
   }
 
   postMessage(data: unknown, transfer: Transferable[] = []): Promise<TWorkerReturnValue> {
