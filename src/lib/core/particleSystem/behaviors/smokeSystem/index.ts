fa6fa62bdf81ee7bae8545a80b42cb1d1d130d6c
Elizaveta Zalomnova <handlessdude@gmail.com>
2025-06-04 00:57:52 +0300

    fix smoke

--- src/lib/core/particleSystem/behaviors/smokeSystem/index.ts
+++ src/lib/core/particleSystem/behaviors/smokeSystem/index.ts
@@ -1,9 +1,8 @@
-import { vec3 } from 'gl-matrix';
 import { ParticleSystem } from '@/lib/core/particleSystem';
-import { ParticlePool } from '@/lib/core/particlePool';
-import { Particle } from '@/lib/core/particle';
 import { generateRandsign } from '../domain/generateRandsign';
 import { SmokeSystemSettings } from './domain/smokeSystemSettings';
+import { MainThreadParticlePool } from '@/lib/core/particlePool/MainThreadParticlePool';
+import { TParticleData } from '@/lib/core/domain/types';
 
 const config = {
   size: {
@@ -37,27 +36,16 @@ const config = {
 export class SmokeSystem extends ParticleSystem {
   readonly settings: SmokeSystemSettings = new SmokeSystemSettings();
 
-  origin: vec3;
-
-  constructor(origin: vec3) {
-    super();
-    this.origin = origin;
-  }
-
-  getSettings(): SmokeSystemSettings {
-    return this.settings;
-  }
-
-  launch(pool: ParticlePool): void {
+  launch(pool: MainThreadParticlePool): void {
     for (let i = 0; i < 100; i++) {
       pool.add({
-        effect: (particle: Particle, dt: number, time: number) => {
+        persistentEffect: (particle: TParticleData, dt: number, time: number) => {
           particle.vz += Math.sin(time * Math.random()) * 0.07;
           particle.vx += Math.sin(time * Math.random()) * 0.07;
         },
-        x: this.origin[0] + config.x.min + generateRandsign() * Math.random() * (config.x.max - config.x.min),
-        y: this.origin[1] + config.y.min + generateRandsign() * Math.random() * (config.y.max - config.y.min),
-        z: this.origin[2] + config.z.min + generateRandsign() * Math.random() * (config.z.max - config.z.min),
+        x: this.settings.origin[0] + config.x.min + generateRandsign() * Math.random() * (config.x.max - config.x.min),
+        y: this.settings.origin[1] + config.y.min + generateRandsign() * Math.random() * (config.y.max - config.y.min),
+        z: this.settings.origin[2] + config.z.min + generateRandsign() * Math.random() * (config.z.max - config.z.min),
         mass: config.mass,
         gravity: Math.random(),
         size: config.size.min + Math.random() * (config.size.max - config.size.min),
